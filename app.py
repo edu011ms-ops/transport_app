b8ba41e39277c05cb49704938af64e4329e841ea
edu011ms-ops <eduardo18matos@gmail.com>
2026-04-26 00:16:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ import streamlit as st
 
 st.set_page_config(layout="centered")
 
-st.title("")
+st.title("Gerador de Imagens")
 
-# CSS para centralizar e aumentar botão
+# estado para controlar se já clicou
+if "mostrar" not in st.session_state:
+    st.session_state.mostrar = False
+
+if "imagem_atual" not in st.session_state:
+    st.session_state.imagem_atual = 1
+
+# estilo do botão
 st.markdown("""
     <style>
     .center {
@@ -13,20 +20,36 @@ st.markdown("""
         margin-top: 50px;
     }
     div.stButton > button {
-        width: 700px;
+        width: 300px;
         height: 100px;
         font-size: 24px;
     }
     </style>
 """, unsafe_allow_html=True)
 
-# centralizar botão
+# botão principal
 st.markdown('<div class="center">', unsafe_allow_html=True)
 
-clicou = st.button("MOSTRAR IMAGEM")
+if st.button("GERAR IMAGEM"):
+    st.session_state.mostrar = True
 
 st.markdown('</div>', unsafe_allow_html=True)
 
-# quando clicar
-if clicou:
-    st.image("foto1.png", caption="", use_container_width=True)
+# mostrar imagem
+if st.session_state.mostrar:
+    
+    # escolher imagem
+    if st.session_state.imagem_atual == 1:
+        st.image("foto1.png", use_container_width=True)
+    else:
+        st.image("foto2.png", use_container_width=True)
+
+    # botão "OU"
+    if st.button("OU"):
+        # alterna imagem
+        if st.session_state.imagem_atual == 1:
+            st.session_state.imagem_atual = 2
+        else:
+            st.session_state.imagem_atual = 1
+
+        st.rerun()
